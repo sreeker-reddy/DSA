9d3b2d4793fcc9f39a6e902cac6792436b298d92
sreeker-reddy <116924654+sreeker-reddy@users.noreply.github.com>
2026-03-26 01:48:08 -0700

    Create 1624-CloneBinaryTreeWithRandomPointer

---
+++ 1624-CloneBinaryTreeWithRandomPointer/1624-CloneBinaryTreeWithRandomPointer.java
@@ -0,0 +1,35 @@
+// Last updated: 3/26/2026, 1:48:08 AM
+/**
+ * Definition for Node.
+ * public class Node {
+ *     int val;
+ *     Node left;
+ *     Node right;
+ *     Node random;
+ *     Node() {}
+ *     Node(int val) { this.val = val; }
+ *     Node(int val, Node left, Node right, Node random) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *         this.random = random;
+ *     }
+ * }
+ */
+
+class Solution {
+    Map<Node, NodeCopy> map = new HashMap<>();
+    public NodeCopy copyRandomBinaryTree(Node root) {
+        if(root == null)
+            return null;
+        if(map.containsKey(root))
+            return map.get(root);
+        
+        NodeCopy newNode = new NodeCopy(root.val);
+        map.put(root,newNode);
+        newNode.left = copyRandomBinaryTree(root.left);
+        newNode.right = copyRandomBinaryTree(root.right);
+        newNode.random = copyRandomBinaryTree(root.random);
+        return newNode;
+    }
+}
