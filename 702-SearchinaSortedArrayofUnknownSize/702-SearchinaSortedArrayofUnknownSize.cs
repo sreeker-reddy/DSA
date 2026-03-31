dc6c1de568e2ff8246578aa6c9dba6d82fc32ea0
sreeker-reddy <116924654+sreeker-reddy@users.noreply.github.com>
2026-03-31 14:06:51 -0700

    File updated at 3/31/2026, 2:06:51 PM

--- 702-SearchinaSortedArrayofUnknownSize/702-SearchinaSortedArrayofUnknownSize.cs
+++ 702-SearchinaSortedArrayofUnknownSize/702-SearchinaSortedArrayofUnknownSize.cs
@@ -1,4 +1,4 @@
-// Last updated: 3/31/2026, 2:05:37 PM
+// Last updated: 3/31/2026, 2:06:51 PM
 1/**
 2 * // This is ArrayReader's API interface.
 3 * // You should not implement it, or speculate about its implementation
@@ -18,30 +18,25 @@
 17            high = 2*high;
 18        }
 19
-20        return BinarySearch(reader,target,low,high);
-21    }
-22
-23    private int BinarySearch(ArrayReader reader, int target, int low,int high)
-24    {
-25        while(low<=high)
-26        {
-27            int mid = low+(high-low)/2;
-28            if(reader.get(mid)==target)
-29            {
-30                return mid;
-31            }
-32            else
-33            {
-34                if(reader.get(mid)<target)
-35                {
-36                    low = mid+1;
-37                }
-38                else
-39                {
-40                    high = mid-1;
-41                }
-42            }
-43        }
-44        return -1;
-45    }
-46}
+20        int mid;
+21        while(low<=high)
+22        {
+23            mid = low + (high-low)/2;
+24            if(reader.get(mid)==target)
+25            {
+26                return mid;
+27            }
+28
+29            if(reader.get(mid)>target)
+30            {
+31                high = mid-1;
+32            }
+33            else
+34            {
+35                low = mid+1;
+36            }
+37        }
+38
+39        return -1;
+40    }
+41}
