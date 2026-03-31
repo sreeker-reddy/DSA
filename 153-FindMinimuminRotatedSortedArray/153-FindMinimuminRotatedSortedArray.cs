cf40c72ec35b53684bbab522b3981e18b2999ff6
sreeker-reddy <116924654+sreeker-reddy@users.noreply.github.com>
2026-03-31 15:01:50 -0700

    File updated at 3/31/2026, 3:01:50 PM

--- 153-FindMinimuminRotatedSortedArray/153-FindMinimuminRotatedSortedArray.cs
+++ 153-FindMinimuminRotatedSortedArray/153-FindMinimuminRotatedSortedArray.cs
@@ -1,30 +1,35 @@
-// Last updated: 3/31/2026, 2:55:43 PM
+// Last updated: 3/31/2026, 3:01:50 PM
 1public class Solution {
 2    public int FindMin(int[] nums) {
-3        int low = 0, high = nums.Length-1;
-4
-5        while(low<=high)
-6        {
-7            if(nums[low]<=nums[high]) return nums[low];
-8
-9            int mid = low+(high-low)/2;
-10            
-11            if((mid == 0 || nums[mid] < nums[mid - 1]) &&  nums[mid] < nums[mid + 1])
-12            {
-13                return nums[mid];
-14            }
-15            else
+3        int low = 0;
+4        int high = nums.Length-1;
+5        int mid;
+6        
+7        while(low<=high)
+8        {
+9            if(nums[low]<=nums[high])
+10            {
+11                return nums[low];
+12            }
+13
+14            mid = low + (high-low)/2;
+15            if((mid==0 || nums[mid]<nums[mid-1]) && (nums[mid]<nums[mid+1]))
 16            {
-17                if(nums[low]<=nums[mid])
-18                {
-19                    low = mid+1;
-20                }
-21                else
+17                return nums[mid];
+18            }
+19            else
+20            {
+21                if(nums[mid]>=nums[low])
 22                {
-23                    high = mid-1;
+23                    low = mid+1;
 24                }
-25            } 
-26        }
-27        return -1;
-28    }
-29}
+25                else
+26                {
+27                    high = mid-1;
+28                }
+29            }
+30        }
+31
+32        return -1;
+33    }
+34}
