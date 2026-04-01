00a8d86f2306b966d971e5d329583b7cc4b7ab95
sreeker-reddy <116924654+sreeker-reddy@users.noreply.github.com>
2026-04-01 12:36:44 -0700

    File updated at 4/1/2026, 12:36:44 PM

--- 409-LongestPalindrome/409-LongestPalindrome.cs
+++ 409-LongestPalindrome/409-LongestPalindrome.cs
@@ -1,34 +1,22 @@
-// Last updated: 4/1/2026, 12:32:56 PM
+// Last updated: 4/1/2026, 12:36:44 PM
 1public class Solution {
 2    public int LongestPalindrome(string s) {
 3        int maxLength = 0;
-4
-5        Dictionary<char,int> dict = new();
-6        bool oddExists  = false;
-7        foreach(char c in s)
-8        {
-9            if(!dict.ContainsKey(c))
-10                dict.Add(c,1);
-11            else
-12                dict[c]++;
-13        }
-14
-15        foreach(var d in dict)
-16        {
-17            if(d.Value%2==0)
-18            {
-19                maxLength+=d.Value;
-20            }
-21            else
-22            {
-23                maxLength+=d.Value-1;
-24                oddExists = true;
-25            }
-26        }
-27
-28        if(oddExists)
-29            return maxLength+1;
-30        else
-31            return maxLength;
-32    }
-33}
+4        HashSet<char> hs = new();
+5
+6        foreach(char c in s)
+7        {
+8            if(hs.Contains(c))
+9            {
+10                maxLength+=2;
+11                hs.Remove(c);
+12            }
+13            else
+14            {
+15                hs.Add(c);
+16            }
+17        }
+18
+19        return maxLength + (hs.Count==0?0:1);
+20    }
+21}
