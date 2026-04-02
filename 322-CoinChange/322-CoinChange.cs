913ef2dac95ea3b92c6dd62dc77db7ea1d76ef4f
sreeker-reddy <116924654+sreeker-reddy@users.noreply.github.com>
2026-04-02 01:02:02 -0700

    File updated at 4/2/2026, 1:02:02 AM

--- 322-CoinChange/322-CoinChange.cs
+++ 322-CoinChange/322-CoinChange.cs
@@ -1,38 +1,37 @@
-// Last updated: 4/2/2026, 12:59:16 AM
+// Last updated: 4/2/2026, 1:02:02 AM
 1public class Solution {
 2    public int CoinChange(int[] coins, int amount) {
-3        int m = amount;
-4        int n = coins.Length;
-5
-6        int[][] dp = new int[n+1][];
-7
-8        for(int i=0;i<dp.Length;i++)
-9        {
-10            dp[i] = new int[m+1];
-11        }
-12        dp[0][0]=0;
-13        for(int i=1;i<=m;i++)
-14        {
-15            dp[0][i] = m+1;
-16        }
-17
-18        for(int i=1;i<=n;i++)
-19        {
-20            dp[i][0] = 0;
-21        }
-22
-23        for(int i = 1;i<=n;i++)
-24        {
-25            for(int j=1;j<=m;j++)
-26            {
-27                if(j<coins[i-1])
-28                    dp[i][j] = dp[i-1][j];
-29                else
-30                    dp[i][j] = Math.Min(dp[i-1][j],1+dp[i][j-coins[i-1]]);
-31            }
-32        }
-33
-34        if(dp[n][m]>m) return -1;
-35        else return dp[n][m];
-36    }
-37}
+3        int n = coins.Length;
+4        int m = amount;
+5        int[][] dp = new int[n+1][];
+6
+7        for(int i=0;i<=n;i++)
+8            dp[i] = new int[m+1];
+9
+10        dp[0][0] = 0;
+11
+12        for(int j=1;j<=m;j++)
+13        {
+14            dp[0][j] = m+1;
+15        }
+16
+17        for(int i=1;i<=n;i++)
+18        {
+19            dp[i][0] = 0;
+20        }
+21
+22        for(int i=1;i<=n;i++)
+23        {
+24            for(int j=1;j<=m;j++)
+25            {
+26                if(j<coins[i-1])
+27                    dp[i][j] = dp[i-1][j];
+28                else
+29                    dp[i][j] = Math.Min(dp[i-1][j], 1+dp[i][j-coins[i-1]]);
+30            }
+31        }
+32
+33        if(dp[n][m]>=m+1) return -1;
+34        return dp[n][m];
+35    }
+36}
